9eb06a4111e7af362477eea776323b68bb0bad43
tqer39 <tqer39@gmail.com>
2023-06-03 14:01:00 +0000

    🍱 Add ArticleList Component

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -2,11 +2,13 @@ import { FC } from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import RightPane from './RightPane';
+import ArticleList from './ArticleList';
 
 const Layout: FC = () => {
   return (
     <>
       <Header blogTitle={'engineering logs'} />
+      <ArticleList />
       <RightPane />
       <Footer />
     </>
