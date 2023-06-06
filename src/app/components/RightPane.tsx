396cf357d2dadde1110989d1c73b206d75d24489
tqer39 <tqer39@gmail.com>
2023-06-06 15:46:40 +0000

    🐛 Fixed to not display Warning in Avatar

--- src/app/components/RightPane.tsx
+++ src/app/components/RightPane.tsx
@@ -1,12 +1,16 @@
 import { FC } from 'react';
 import Avatar from './Avatar';
 import Profile from './Profile';
+import getGravatarUrl from '../utils/gravatar';
 
-const RightPane: FC = () => {
+const RightPane: FC = async () => {
+  const email = process.env.GRAVATAR_EMAIL_ADDRESS || '';
+  const avatarUrl = await getGravatarUrl(email);
+
   return (
     <div className="right-pane">
-      <Avatar />
-      <Profile name='tqer39' bio='プロフィールほげほげ' />
+      <Avatar avatarUrl={avatarUrl} />
+      <Profile name="tqer39" bio="プロフィールほげほげ" />
     </div>
   );
 };
