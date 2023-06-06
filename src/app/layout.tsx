3181a3bb5f84f56b9803c58899a00a8c12de5fb6
tqer39 <tqer39@gmail.com>
2023-06-06 14:55:25 +0000

    ♻️ Fixed as per Chakra UI documentation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
+import Page from './page';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -15,7 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ja">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Page>{children}</Page>
+      </body>
     </html>
   );
 }
