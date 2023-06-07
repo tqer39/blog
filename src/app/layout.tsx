f25a26d4cf1524d620cd89cfc6ada09894b043e0
tqer39 <tqer39@gmail.com>
2023-06-07 21:56:01 +0000

    🐛 Do not pass children to the lower component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body className={inter.className}>
-        <Page>{children}</Page>
+        <Page />
       </body>
     </html>
   );
