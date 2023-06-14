519babc64958e4f7a051e304a438801c9b4e9cf8
tqer39 <tqer39@gmail.com>
2023-06-14 04:23:52 +0000

    ChakraUIを削除

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,8 @@
 import { FC } from 'react';
-import { CacheProvider } from '@chakra-ui/next-js';
-import { ChakraProvider } from '@chakra-ui/react';
 import Articles from './articles/page';
 
 const Page: FC = () => {
-  return (
-    <CacheProvider>
-      <ChakraProvider>
-        <Articles />
-      </ChakraProvider>
-    </CacheProvider>
-  );
+  return <Articles />;
 };
 
 export default Page;
