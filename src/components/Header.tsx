040daf4d90bea4c5d5d6df3896f319f0b01d4689
tqer39 <tqer39@gmail.com>
2023-06-14 21:38:05 +0000

    HeaderコンポーネントのリンクをaタグからLinkコンポーネントに変更する

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,13 +6,13 @@ const Header: FC = () => {
     <div className="bg-white lg:pb-12">
       <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
         <header className="flex items-center justify-between py-4 md:py-8">
-          <a
+          <Link
             href="/"
             className="text-black-800 inline-flex items-center gap-2.5 text-2xl font-bold md:text-3xl"
             aria-label="logo"
           >
             tqer39's blog
-          </a>
+          </Link>
           <nav className="hidden gap-12 lg:flex">
             <Link
               href={'https://bento.me/tqer39'}
