ddb04af0587f477f6f04783d54f538ff881fcc8a
tqer39 <tqer39@gmail.com>
2023-06-17 02:55:06 +0000

    ヘッダーのレイアウトを修正する

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,10 +5,11 @@ import Brand from './Brand';
 
 const Header: FC = () => {
   return (
-    <div className="bg-white dark:bg-black lg:pb-12">
-      <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
-        <header className="flex items-center justify-between py-4 md:py-8">
-          {/*
+    <>
+      <div className="bg-white dark:bg-black lg:pb-12">
+        <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
+          <header className="flex items-center justify-between py-4 md:py-8">
+            {/*
             text-2xl: テキストのサイズを2xlにする
             md:text-4xl: mdサイズ以上の画面の時にテキストのサイズを4xlにする
             font-bold: テキストの太さをboldにする
@@ -18,24 +19,25 @@ const Header: FC = () => {
             mb-20: 下側のマージンを20にする
             mt-8: 上側のマージンを8にする
           */}
-          <div className="inline-flex text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8 text-black-800 dark:text-white-800">
-            <Brand brand={"tqer39's blog"} />
-          </div>
-          <nav className="hidden gap-12 lg:flex">
-            <Link
-              href={'https://bento.me/tqer39'}
-              target="_blank"
-              className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-indigo-500 active:text-indigo-700"
-            >
-              Bio
-            </Link>
-          </nav>
-          <div className="-ml-8 hidden flex-col gap-2.5 sm:flex-row sm:justify-center lg:flex lg:justify-start">
-            <ThemeSwitcher />
-          </div>
-        </header>
+            <div className="inline-flex text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8 text-black-800 dark:text-white-800">
+              <Brand brand={"tqer39's blog"} />
+            </div>
+            <nav className="hidden gap-12 lg:flex">
+              <Link
+                href={'https://bento.me/tqer39'}
+                target="_blank"
+                className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-indigo-500 active:text-indigo-700"
+              >
+                Bio
+              </Link>
+            </nav>
+            <div className="-ml-8 hidden flex-col gap-2.5 sm:flex-row sm:justify-center lg:flex lg:justify-start">
+              <ThemeSwitcher />
+            </div>
+          </header>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
