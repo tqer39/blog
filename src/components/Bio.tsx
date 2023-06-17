5402348058c3155ab85d05168bbaa795a25db241
tqer39 <tqer39@gmail.com>
2023-06-17 02:57:46 +0000

    「Bio」コンポーネントを追加する

---
+++ src/components/Bio.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+import { FC } from 'react';
+
+const Brand: FC = () => {
+  return (
+    <>
+      <Link href={'https://bento.me/tqer39'} target="_blank">
+        Bio
+      </Link>
+    </>
+  );
+};
+
+export default Brand;
