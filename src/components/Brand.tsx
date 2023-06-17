4ba41a53d1b49616af69791ba81220979e941c2a
tqer39 <tqer39@gmail.com>
2023-06-17 02:53:36 +0000

    ブランドのテキストスタイルを変更する

--- src/components/Brand.tsx
+++ src/components/Brand.tsx
@@ -7,19 +7,11 @@ type BrandProps = {
 
 const Brand: FC<BrandProps> = ({ brand }) => {
   return (
-    // text-2xl: テキストのサイズを2xlにする
-    // md:text-4xl: mdサイズ以上の画面の時にテキストのサイズを4xlにする
-    // font-bold: テキストの太さをboldにする
-    // tracking-tight: 文字間を詰める
-    // md:tracking-tighter: mdサイズ以上の画面の時に文字間をさらに詰める
-    // leading-tight: 行間を詰める
-    // mb-20: 下側のマージンを20にする
-    // mt-8: 上側のマージンを8にする
-    <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8 text-black-800 dark:text-white-800">
-      <Link href="/" className="inline-flex" aria-label="logo">
+    <>
+      <Link href="/" aria-label="logo">
         {brand}
       </Link>
-    </h2>
+    </>
   );
 };
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,6 +8,17 @@ const Header: FC = () => {
     <div className="bg-white lg:pb-12">
       <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
         <header className="flex items-center justify-between py-4 md:py-8">
+          {/*
+            text-2xl: テキストのサイズを2xlにする
+            md:text-4xl: mdサイズ以上の画面の時にテキストのサイズを4xlにする
+            font-bold: テキストの太さをboldにする
+            tracking-tight: 文字間を詰める
+            md:tracking-tighter: mdサイズ以上の画面の時に文字間をさらに詰める
+            leading-tight: 行間を詰める
+            mb-20: 下側のマージンを20にする
+            mt-8: 上側のマージンを8にする
+          */}
+          <div className="inline-flex text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8 text-black-800 dark:text-white-800">
           <Brand brand={"tqer39's blog"} />
           <nav className="hidden gap-12 lg:flex">
             <Link
