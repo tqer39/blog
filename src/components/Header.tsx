49c9b65be6d2fcb4374d0a7b052e51aadc748e12
tqer39 <tqer39@gmail.com>
2023-06-17 03:10:46 +0000

    ヘッダーのナビゲーションの色を変更する

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -22,7 +22,7 @@ const Header: FC = () => {
             <div className="inline-flex text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8 text-black-800 dark:text-white-800">
               <Brand brand={"tqer39's blog"} />
             </div>
-            <nav className="hidden lg:flex text-lg mb-8 font-semibold text-gray-600 transition duration-100 hover:text-indigo-500 active:text-indigo-700">
+            <nav className="hidden lg:flex text-lg mb-8 font-semibold text-gray-600 dark:text-gray-50 transition duration-100 hover:text-indigo-500 active:text-indigo-700">
               <Bio />
             </nav>
             <div className="-ml-8 hidden flex-col gap-2.5 sm:flex-row sm:justify-center lg:flex lg:justify-start">
