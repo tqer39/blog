e93bd56096cbf5a56733f7fc70323f35c17af763
tqer39 <tqer39@gmail.com>
2023-06-19 07:42:24 +0000

    マークダウンをHTMLに変換する関数を追加

--- src/lib/markdown.ts
+++ src/lib/markdown.ts
@@ -1,8 +1,17 @@
-import { readFileSync } from "fs";
-import matter from "gray-matter";
+import { readFileSync } from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+import { remark }  from 'remark';
+import html from 'remark-html';
 
-export const getMarkdown = (filePath: string) => {
-  const fileContents = readFileSync(filePath, "utf8");
+export const getMarkdown = async (id: string) => {
+  const filePath = path.join(process.cwd(), 'src/contents', `${id}.md`);
+  const fileContents = readFileSync(filePath, 'utf8');
+  return fileContents;
+};
 
-  return matter(fileContents);
+export const markdownToHtml = async (markdownContent: string) => {
+  const matterData = matter(markdownContent);
+  const result = await remark().use(html).process(matterData.content);
+  return result.toString();
 };
