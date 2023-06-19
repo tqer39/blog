5a4ed396f2e9622ba5431892a3ff69beecf3925b
tqer39 <tqer39@gmail.com>
2023-06-19 13:35:18 +0000

    テーマ変更のプルダウンをボタンに変更

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -1,24 +1,12 @@
-'use client';
-
-import { useEffect, useState } from 'react';
-import { useTheme } from 'next-themes';
+import { ChangeThemeButton } from './ChangeThemeButton';
 
 const ThemeSwitcher = () => {
-  const { theme, setTheme } = useTheme();
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) return null;
-
   return (
-    <select value={theme} onChange={(e) => setTheme(e.target.value)}>
-      <option value="system">System</option>
-      <option value="dark">Dark</option>
-      <option value="light">Light</option>
-    </select>
+    <div className="flex items-center justify-between">
+      <span className="group inline-flex items-center text-xl font-medium pl-2 pr-3 py-2">
+        <ChangeThemeButton />
+      </span>
+    </div>
   );
 };
 
