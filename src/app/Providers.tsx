a5af031f079fb52c88fff845f572f3ba0043eed8
tqer39 <tqer39@gmail.com>
2023-06-22 07:46:28 +0000

    Providers.tsxを修正: インポート順を変更

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,8 +1,8 @@
 'use client';
 
-import { useEffect, useState } from 'react';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import { ThemeProviderProps } from 'next-themes/dist/types';
+import { useEffect, useState } from 'react';
 
 export function Providers({ children, ...props }: ThemeProviderProps) {
   const [mounted, setMounted] = useState(false);
