a9933813770c82dc5f5235d4244862ac14e01fa1
tqer39 <tqer39@gmail.com>
2023-06-22 07:54:43 +0000

    テーマ切り替えボタンのスタイル修正

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -3,7 +3,7 @@ import { ChangeThemeButton } from './ChangeThemeButton';
 const ThemeSwitcher = () => {
   return (
     <div className="flex items-center justify-between">
-      <span className="group inline-flex items-center text-xl font-medium pl-2 pr-3 py-2">
+      <span className="group inline-flex items-center py-2 pl-2 pr-3 text-xl font-medium">
         <ChangeThemeButton />
       </span>
     </div>
