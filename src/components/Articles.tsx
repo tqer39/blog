43fc888cd77c5048331f522aa7913e34326ed2de
tqer39 <tqer39@gmail.com>
2023-06-22 07:47:59 +0000

    Layout import順序変更

--- src/components/Articles.tsx
+++ src/components/Articles.tsx
@@ -1,6 +1,6 @@
 import { BlogPost, getArticleMetadata } from '../lib/notion';
-import Layout from './Layout';
 import BlogCard from './BlogCard';
+import Layout from './Layout';
 
 const Articles = async () => {
   const articles: BlogPost = await getArticleMetadata();
@@ -11,8 +11,8 @@ const Articles = async () => {
 
   return (
     <Layout>
-      <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
-        <div className="grid lg:grid-cols-2 lg:gap-y-16 gap-10">
+      <div className="mx-auto max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14">
+        <div className="grid gap-10 lg:grid-cols-2 lg:gap-y-16">
           {articles && articles.length > 0 ? (
             articles.map((article) => {
               return (
