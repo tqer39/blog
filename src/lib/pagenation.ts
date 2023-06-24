764cda5eb728b7fe5f411707aadd41c8b7da1b7c
tqer39 <tqer39@gmail.com>
2023-06-24 23:55:50 +0000

    ページネーション機能を追加

---
+++ src/lib/pagenation.ts
@@ -0,0 +1,44 @@
+const pagination = (current: number, max: number): (number | string)[] => {
+  const result: (number | string)[] = [];
+
+  if (max <= 7) {
+    for (let i = 1; i <= max; i++) {
+      result.push(i);
+    }
+  } else {
+    if (current <= 4) {
+      for (let i = 1; i <= 5; i++) {
+        result.push(i);
+      }
+      result.push('...');
+      result.push(max);
+    } else if (current >= max - 3) {
+      result.push(1);
+      result.push('...');
+      for (let i = max - 4; i <= max; i++) {
+        result.push(i);
+      }
+    } else {
+      result.push(1);
+      result.push('...');
+      for (let i = current - 1; i <= current + 1; i++) {
+        result.push(i);
+      }
+      result.push('...');
+      result.push(max);
+    }
+  }
+
+  return result;
+};
+
+export default pagination;
+
+// [DEBUG]
+// console.log(pagination(4, 9)); // [1, "...", 3, 4, 5, "...", 9]
+// console.log(pagination(1, 7)); // [1, 2, 3, 4, 5, 6, 7]
+// console.log(pagination(4, 7)); // [1, 2, 3, 4, 5, 6, 7]
+// console.log(pagination(4, 8)); // [1, 2, 3, 4, 5, '...', 8]
+// console.log(pagination(5, 8)); // [1, '...', 4, 5, 6, 7, 8]
+// console.log(pagination(5, 9)); // [1, '...', 4, 5, 6, '...', 9]
+// console.log(pagination(20, 20)); // [1, "...", 16, 17, 18, 19, 20]
