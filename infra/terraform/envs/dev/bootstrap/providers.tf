a83c2851e948e471a9e5a0b3be8ccf634d257f90
tqer39 <tqer39@gmail.com>
2025-12-31 00:48:58 +0900

    feat: update AWS provider configuration to use dynamic region and project-specific tags

--- infra/terraform/envs/dev/bootstrap/providers.tf
+++ infra/terraform/envs/dev/bootstrap/providers.tf
@@ -1,11 +1,12 @@
 provider "aws" {
-  region = "ap-northeast-1"
-
+  region = local.config.aws.region
   default_tags {
     tags = {
-      Project    = "blog"
-      ManagedBy  = "terraform"
-      Repository = "github.com/tqer39/blog"
+      env                                        = local.config.environments.dev.name
+      product                                    = local.config.project.name
+      repository                                 = local.config.project.repository
+      IaC                                        = local.config.aws.IaC
+      "${local.config.project.name}:source_path" = "./infra/terraform/envs/${local.config.environments.dev.name}/bootstrap"
     }
   }
 }
