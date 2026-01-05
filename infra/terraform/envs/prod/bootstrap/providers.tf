fcab67a886e1c0a3f30d71b80eeb09440da60bb3
tqer39 <tqer39@gmail.com>
2026-01-06 01:38:39 +0900

    feat: simplify AWS provider tags in Terraform configuration

--- infra/terraform/envs/prod/bootstrap/providers.tf
+++ infra/terraform/envs/prod/bootstrap/providers.tf
@@ -1,12 +1,11 @@
 provider "aws" {
   region = local.config.aws.region
+
   default_tags {
     tags = {
-      env                                        = local.config.environments.dev.name
-      product                                    = local.config.project.name
-      repository                                 = local.config.project.repository
-      IaC                                        = local.config.aws.IaC
-      "${local.config.project.name}:source_path" = "./infra/terraform/envs/${local.config.environments.dev.name}/bootstrap"
+      Project    = local.config.project.name
+      ManagedBy  = "terraform"
+      Repository = "github.com/${local.config.project.organization}/${local.config.project.repository}"
     }
   }
 }
