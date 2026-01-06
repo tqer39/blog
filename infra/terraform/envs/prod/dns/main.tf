52a1a27eec954675b36ec89ecc4d5049dd9e8cbc
tqer39 <tqer39@gmail.com>
2026-01-07 04:29:42 +0900

    fix(dns): remove unused terraform_remote_state data block for frontend

--- infra/terraform/envs/prod/dns/main.tf
+++ infra/terraform/envs/prod/dns/main.tf
@@ -4,21 +4,11 @@ locals {
   domain     = local.config.project.domain
 }
 
-# frontend の state から CNAME ターゲットを取得
-data "terraform_remote_state" "frontend" {
-  backend = "s3"
-  config = {
-    bucket = "terraform-tfstate-tqer39-072693953877-ap-northeast-1"
-    key    = "blog/infra/terraform/envs/prod/prod-frontend.tfstate"
-    region = "ap-northeast-1"
-  }
-}
-
 module "dns" {
   source = "../../../modules/cloudflare-dns"
 
-  zone_id      = var.cloudflare_zone_id
-  subdomain    = local.env_config.subdomain
-  vercel_cname = data.terraform_remote_state.frontend.outputs.vercel_cname_target
-  repository   = local.config.project.repository
+  zone_id    = var.cloudflare_zone_id
+  subdomain  = local.env_config.subdomain
+  repository = local.config.project.repository
+  # vercel_cname uses default "cname.vercel-dns.com"
 }
