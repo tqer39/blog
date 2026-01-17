62b32470def558c7cbe240dc58046e69bf561083
Takeru O'oyama <tqer39@gmail.com>
2026-01-17 19:39:27 +0900

    🌐 Cloudflare R2のlocation hintに関する問題を文書化し、バケット名を更新 (#296)
    * feat: add initial vercel configuration for Next.js deployment
    
    * feat: 画像設定の追加とキャッシュTTLの設定
    
    * fix: R2バケット名を更新して画像ストレージを明確化
    
    * feat: Cloudflare R2のlocation hintに関する問題と解決策を文書化

--- infra/terraform/envs/prod/cms-api/main.tf
+++ infra/terraform/envs/prod/cms-api/main.tf
@@ -18,7 +18,7 @@ module "cms_r2" {
   source = "../../../modules/cloudflare-r2"
 
   account_id  = var.cloudflare_account_id
-  bucket_name = "blog-assets-prod"
+  bucket_name = "blog-images-prod-v2"
   location    = "apac"
 }
 
